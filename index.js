bb2c542c32f66467ac396cf4a9cae36c8bfeb2c3
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-11-19 21:48:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    if(req.url==='/'){
-        res.write("My first API response");
-        res.end();
-    }
-    if(req.url==='/login'){
-        res.write("My first login response");
-        res.end();
-    }
-});
 
-console.log("Listening for client");    
-server.listen(3000);
-
+const mongoos = require('mongo');
